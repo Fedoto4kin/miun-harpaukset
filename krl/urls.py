b6767a03f60d61ff60c2a64b2a527713df8d65ab
F Anatole <fedoto4kin@gmail.com>
2020-02-14 22:35:22 +0000

    url routers

--- krl/urls.py
+++ krl/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.conf import settings
+from django.conf.urls.static import static
 from krl import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    
+    
     path('admin/', admin.site.urls),
     path('api/v0/', include('api_v0.urls')),
-    path('lexicon/', include('main.urls')),
+    path('', include('main.urls')),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+#    path('lexicon/', include('main.urls')),
 ]
 
 if settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    re_path(r'^', IndexView.as_view()),
+    re_path(r'', IndexView.as_view()),
+
+#	path('', IndexView.as_view()),
+#	path('<path:resource>', IndexView.as_view()),
+
 ]
