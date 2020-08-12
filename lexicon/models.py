6f62d119f46defccaf4b01611f70ab152a675edd
F Anatole <fedoto4kin@gmail.com>
2020-08-12 18:40:46 +0000

    Fix remove search prepare

--- lexicon/models.py
+++ lexicon/models.py
@@ -1,39 +1,40 @@
+import re
 from django.db import models
 from django.utils.translation import gettext as _
-from django.core.validators import MinValueValidator, MaxValueValidator
 from slugify import slugify
 from uuid import uuid4
 from django.utils.text import Truncator
 
+KRL_ABC = 'ABCČDEFGHIJKLMNOPRSŠZŽTUVYÄÖ'
 
-KRL_ABC = 'ABCČDEFGHIJKLMNOPRSŠZŽTUVYÄÖ';
 
 class Speech(models.Model):
 
-    def sound_upload_path(instance, filename):
+    def sound_upload_path(self, filename):
         filename = "%s.%s" % (uuid4(), filename.split('.')[-1])
-        dir_id = ord(instance.text[0]) % 10
+        dir_id = ord(self.text[0]) % 10
         return 'lexicon/speech_{0}/{1}'.format(dir_id, filename)
 
     mp3 = models.FileField(
-                upload_to=sound_upload_path,
-                null=True,
-                blank=True,
-                editable=True,
+        upload_to=sound_upload_path,
+        null=True,
+        blank=True,
+        editable=True,
     )
-    text = models.TextField(blank=False,  null = True, db_index = True)
+    text = models.TextField(blank=False, null=True, db_index=True)
 
     def __str__(self):
         return Truncator(self.text).words(1)
 
+
 class Pos(models.Model):
-    
+
     abbr = models.CharField(max_length=32)
     name_ru = models.CharField(max_length=32, default='?')
     name_fi = models.CharField(max_length=32, default='?')
 
     def __str__(self):
-           return self.abbr
+        return self.abbr
 
     class Meta:
         verbose_name = 'Šanaloukka'
@@ -52,7 +53,12 @@ class Word(models.Model):
         return KRL_ABC
 
     def __str__(self):
-        return self.word
+        return self.base_set.get(num=0).base
+
+    @staticmethod
+    def search_prepare(string):
+        return string.lower().replace('ü', 'y').replace('\'', '') #TODO: add filter non-aphabeth and whitespace
+
 
     class Meta:
         verbose_name = 'Šana'
@@ -60,17 +66,16 @@ class Word(models.Model):
 
 
 class Definition(models.Model):
-
     LANGUAGE = (
         ('ru', _('Hormiksi')),
         ('fi', _('Šuomekši')),
     )
-    
-    word = models.ForeignKey(Word, unique=False, on_delete=models.CASCADE,  related_name='definition_set')
-    lang =  models.CharField(max_length=32, choices=LANGUAGE)
+
+    word = models.ForeignKey(Word, unique=False, on_delete=models.CASCADE, related_name='definition_set')
+    lang = models.CharField(max_length=32, choices=LANGUAGE)
     definition = models.TextField(blank=True)
     definition_lcase = models.TextField(blank=True)
-    
+
     class Meta:
         ordering = ['-lang']
 
@@ -78,26 +83,69 @@ class Definition(models.Model):
         self.definition_lcase = self.definition.lower()
         super(Definition, self).save(*args, **kwargs)
 
-    
+
 class Base(models.Model):
-    
+
+    BASES = (
+        (0, 0),
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+        (7, 7),
+    )
+
     word = models.ForeignKey(Word, unique=False, on_delete=models.CASCADE, related_name='base_set')
-    num = models.IntegerField(choices=((int(x), x) for x in range(0,8)))
+    num = models.IntegerField(choices=BASES)
     base = models.CharField(max_length=128)
     base_slug = models.CharField(max_length=128, db_index=True, blank=True)
-    base_slug_diacrit = models.CharField(max_length=128, db_index=True, blank=True)
-    
+    base_slug_diacritic = models.CharField(max_length=128, db_index=True, blank=True)
+
     @staticmethod
     def get_krl_abc():
         return KRL_ABC
+
+    def __str__(self):
+        return self.base
 
     class Meta:
         unique_together = ("num", "base", "word")
 
     def krl_slugify(self, string):
-        return ''.join([i for i in string.lower().replace('ü','y') if i in self.get_krl_abc().lower()])
-    
+        return ''.join([i for i in string.lower().replace('ü', 'y') if i in self.get_krl_abc().lower()])
+
     def save(self, *args, **kwargs):
         self.base_slug = slugify(self.base)
-        self.base_slug_diacrit = self.krl_slugify(self.base)
+        self.base_slug_diacritic = self.krl_slugify(self.base)
         super(Base, self).save(*args, **kwargs)
+
+
+class GrammarForm(models.Model):
+    name = models.CharField(max_length=128, db_index=True, blank=True)
+    pos = models.ManyToManyField(Pos)
+    pre = models.CharField(max_length=128, blank=True, default='')
+    end = models.CharField(max_length=128, blank=True, default='')
+    base = models.IntegerField(choices=Base.BASES)
+    # lambda function (?)
+    rule = models.CharField(max_length=1024, blank=True, default='')
+    hidden = models.BooleanField(default=False)
+
+
+class WordForm(models.Model):
+
+    @staticmethod
+    def vowel_garmonize(suff):
+        """
+        Returns harmonized suffix by vovel of base
+        if base contains a, o or u -> a suff
+        else ä, ö and y in suff
+        """
+        # TODO
+        return suff
+
+    word = models.ForeignKey(Word, unique=False, on_delete=models.CASCADE, related_name='form_set')
+    form = models.ForeignKey(GrammarForm, unique=False, on_delete=models.CASCADE, related_name='form')
+    form_slug = models.CharField(max_length=128, db_index=True, blank=True)
+    form_slug_diacritic = models.CharField(max_length=128, db_index=True, blank=True)
