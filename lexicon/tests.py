678132ed385dfc76ee7eb36f082b73055c0baca9
F Anatole <fedoto4kin@gmail.com>
2022-11-19 08:58:39 +0000

    prepare for moving to new server

--- lexicon/tests.py
+++ lexicon/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Word, Base
+
+class WordTestClass(TestCase):
+
+    def test_search_prepare(self):
+        search = 'ABCČDEFGHIJKLMNOPRSŠZŽTUVYÄÖüÜ\'’'
+        self.assertEqual(Word.search_prepare(search), 'abcčdefghijklmnoprsšzžtuvyäöyy')
+
+    def test_krl_slugify(self):
+        search = 'ABCČDEFGHIJKLMNOPRSŠZŽTUVYÄÖÜ\'’'
+        self.assertEqual(Base.krl_slugify(Base, search), 'abcčdefghijklmnoprsšzžtuvyäöy')
