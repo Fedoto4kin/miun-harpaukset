c1e37519d7f1a1a9fc9dbc5b50d48c145448473e
Anatole F <fedoto4kin@gmail.com>
2025-02-17 12:56:02 +0300

    Catch titlw when router handle

--- client/src/components/LessonsComponent.vue
+++ client/src/components/LessonsComponent.vue
@@ -73,7 +73,7 @@ export default {
       console.error('Error loading lessons:', error);
     } finally {
       this.loading = false;
-      document.title = this.title;
+      document.title =  this.activeLesson.full_name;
     }
   },
   computed: {
