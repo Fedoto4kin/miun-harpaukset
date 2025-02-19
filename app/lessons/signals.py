82e86287ccad8c76ef227d44cc9a12dcaad8296e
Anatole F <fedoto4kin@gmail.com>
2025-02-19 21:25:03 +0300

    feat(signals): update tag handling code
    Updated signal handling code for LessonSpeech and Module models:
    - post_save and post_delete signals for LessonSpeech now call the update_listen_tag_on_speech_change function
    - Added post_save signal for Module, which calls the update_listen_tag_on_module_save function
    - The check_and_update_listen_tag function now adds or removes the LISTEN_TAG_CODE tag depending on the presence of LessonSpeech records in the Module
    
    Refactoring tag handling code for more precise and convenient management.

--- app/lessons/signals.py
+++ app/lessons/signals.py
@@ -6,30 +6,30 @@ LISTEN_TAG_CODE = 'listen'
 KEY_TAG_CODE = 'key'
 
 @receiver(post_save, sender=LessonSpeech)
-def add_tell_tag_on_speech_save(sender, instance, **kwargs):
-    if instance.content_object and isinstance(instance.content_object, Module):
-        module = instance.content_object
-        tell_tag, created = Tag.objects.get_or_create(
-            code=LISTEN_TAG_CODE,
-            defaults={
-                'code': LISTEN_TAG_CODE,
-                'name': 'Kuundele da tačkuta! / Kuundele oigie varianta!',
-                'hint_russian': 'Послушай и повтори / Послушай правильный вариант',
-                'hint_finnish': 'Kuuntele ja toista / Kuuntele oikea vaihtoehto'
-            }
-        )
-        if instance.mp3:
-            module.tags.add(tell_tag)
-        else:
-            module.tags.remove(tell_tag)
-
 @receiver(post_delete, sender=LessonSpeech)
-def remove_tell_tag_on_speech_delete(sender, instance, **kwargs):
+def update_listen_tag_on_speech_change(sender, instance, **kwargs):
     if instance.content_object and isinstance(instance.content_object, Module):
         module = instance.content_object
-        tell_tag = Tag.objects.filter(code=LISTEN_TAG_CODE).first()
-        if tell_tag:
-            module.tags.remove(tell_tag)
+        check_and_update_listen_tag(module)
+
+@receiver(post_save, sender=Module)
+def update_listen_tag_on_module_save(sender, instance, **kwargs):
+    check_and_update_listen_tag(instance)
+
+def check_and_update_listen_tag(module):
+    tell_tag, _ = Tag.objects.get_or_create(
+        code=LISTEN_TAG_CODE,
+        defaults={
+            'code': LISTEN_TAG_CODE,
+            'name': 'Kuundele da tačkuta! / Kuundele oigie varianta!',
+            'hint_russian': 'Послушай и повтори / Послушай правильный вариант',
+            'hint_finnish': 'Kuuntele ja toista / Kuuntele oikea vaihtoehto'
+        }
+    )
+    if module.lessonspeech_set.exists():
+        module.tags.add(tell_tag)
+    else:
+        module.tags.remove(tell_tag)
 
 @receiver(post_save, sender=Exercise)
 def add_key_tag(sender, instance, created, **kwargs):
@@ -37,15 +37,14 @@ def add_key_tag(sender, instance, created, **kwargs):
         code=KEY_TAG_CODE,
         defaults={
             'code': KEY_TAG_CODE,
-            'name': 'Avuan',
-            'hint_russian': 'Ключ',
-            'hint_finnish': 'Avain'
-        }
+                'name': 'Avuan',
+                'hint_russian': 'Ключ',
+                'hint_finnish': 'Avain'
+            }
     )
     if instance.has_answers_check:
         instance.module.tags.add(key_tag)
     check_and_update_key_tag(instance.module)
-
 
 @receiver(post_delete, sender=Exercise)
 def remove_key_tag(sender, instance, **kwargs):
