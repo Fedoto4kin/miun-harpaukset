979264a59d1780fa17863e65b9832fedb46010b0
Anatole F <fedoto4kin@gmail.com>
2025-02-19 18:04:40 +0300

    feat: enhance deployment script with fixture creation and repository upload option
    - Added a prompt to ask users if they want to create fixtures and upload them to the repository.
    - Implemented conditional commands to dump fixture data for 'lexicon' and 'lessons' after container restart.
    - Included commands to add, commit, and push the fixture files to the git repository with a timestamped message.

--- upgrade.sh
+++ upgrade.sh
@@ -70,4 +70,24 @@ if [ $? -ne 0 ]; then
 fi
 echo "Containers started successfully."
 
+# Asking user if they want to create fixtures and upload to repository
+read -p "Do you want to create fixtures and upload to repository y/n? " choice
+case "$choice" in 
+  y|yes|Y|YES ) 
+    echo "Creating fixtures..."
+    docker compose exec -it web python manage.py dumpdata lexicon --indent 4 > app/lexicon/fixtures/lexicon.json
+    docker compose exec -it web python manage.py dumpdata lessons --indent 4 > app/lessons/fixtures/lessons.json
+    echo "Fixtures created successfully."
+
+    echo "Adding fixtures to git..."
+    git add app/lexicon/fixtures/lexicon.json app/lessons/fixtures/lessons.json
+    git commit -m "[Auto] Update fixtures and commit on $(date '+%Y-%m-%d')"
+    git push
+    echo "Fixtures committed and pushed successfully."
+    ;;
+  * ) 
+    echo "Skipping fixtures creation."
+    ;;
+esac
+
 echo "Deployment tasks completed!"
