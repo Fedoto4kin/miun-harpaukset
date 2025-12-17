f73648bd2b22f93d1338a4dc35045378ea72c0ed
Anatole F <fedoto4kin@gmail.com>
2025-12-17 23:29:33 +0300

    feat: change hint display from hold to click interaction
    - Replace pointer hold with click to show hints in HintButton component
    - Add click-away functionality to hide hints
    - Visual feedback with btn-secondary class when active
    - Update FillBlankTableExerciseComponent for new behavior

--- client/src/components/ui/HintButtonComponent.vue
+++ client/src/components/ui/HintButtonComponent.vue
@@ -1,27 +1,63 @@
 <template>
-    <button
-      class="btn btn-light border-dark"
-      ref="hintButton"
-      @pointerdown="showHint"
-      @pointerup="hideHint"
-      @mouseleave="hideHint"
-      title="Jiävi otvietat"
-    >
-      <font-awesome-icon :icon="['fas', 'key']" />
-    </button>
-  </template>
-  
-  <script>
-  export default {
-    name: 'HintButton',
-    methods: {
-      showHint() {
-        this.$emit('show-hint', true);
-      },
-      hideHint() {
-        this.$emit('show-hint', false);
+  <button
+    class="btn border-dark"
+    :class="isActive ? 'btn-secondary' : 'btn-light'"
+    ref="hintButton"
+    @click="toggleHint"
+    title="Jiävi otvietat"
+  >
+    <font-awesome-icon :icon="['fas', 'key']" />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'HintButton',
+  data() {
+    return {
+      isActive: false,
+      clickListener: null
+    };
+  },
+  methods: {
+    toggleHint() {
+      this.isActive = !this.isActive;
+      this.$emit('show-hint', this.isActive);
+      
+      if (this.isActive) {
+        // Добавляем обработчик клика по всему документу
+        this.clickListener = (e) => {
+          // Если клик не по кнопке подсказки
+          if (!this.$refs.hintButton.contains(e.target)) {
+            this.deactivateHint();
+          }
+        };
+        
+        // Добавляем обработчик с небольшой задержкой, чтобы не сработал сразу
+        setTimeout(() => {
+          document.addEventListener('click', this.clickListener);
+        }, 100);
+      } else {
+        this.removeClickListener();
+      }
+    },
+    
+    deactivateHint() {
+      this.isActive = false;
+      this.$emit('show-hint', false);
+      this.removeClickListener();
+    },
+    
+    removeClickListener() {
+      if (this.clickListener) {
+        document.removeEventListener('click', this.clickListener);
+        this.clickListener = null;
       }
     }
-  };
-  </script>
+  },
   
+  beforeUnmount() {
+    this.removeClickListener();
+  }
+};
+</script>
